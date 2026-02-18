4fce9dd4ceceaa34c22e7599eaf3c6f2fa5605eb
earmahsakyi <sakyi5375@gmail.com>
2026-02-18 19:36:23 +0000

    use connection string for mysql

--- config/mysql.js
+++ config/mysql.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   port: process.env.MYSQLPORT
+// });
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(process.env.DATABASE_URL_MYSQL);
 
 module.exports = pool.promise();
